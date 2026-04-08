bb3f2cc81a0545f40d07b375a5068ee6629a2d56
Antoine Turbis <antur194@ulaval.ca>
2026-04-08 11:26:48 -0400

    début queries

--- sql/03_queries.sql
+++ sql/03_queries.sql
@@ -1,3 +1,16 @@
 USE article_magasin;
-#Produit avec le plus gros rabais
-SELECT * from Produit p where p.rabais = (SELECT MAX(rabais) from Produit);
+#Produits avec les plus gros rabais
+SELECT * from Produit p
+ORDER BY rabais DESC;
+#Produits les plus vendus
+SELECT p.nom, SUM(lc.quantite) AS total_vendu
+FROM LigneDeCommande lc
+JOIN Produit p ON lc.pid = p.pid
+GROUP BY p.pid
+ORDER BY total_vendu DESC;
+#Produits jamais commandés
+SELECT *
+FROM Produit
+WHERE pid NOT IN (
+    SELECT pid FROM LigneDeCommande
+);
