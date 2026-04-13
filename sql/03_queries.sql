181f9c3d1ce17125378772b2e8e98aad30a7d9ca
vreggen007 <157843291+vreggen007@users.noreply.github.com>
2026-04-13 16:25:24 -0400

    Ajustements

--- sql/03_queries.sql
+++ sql/03_queries.sql
@@ -2,19 +2,7 @@ USE article_magasin;
 #Produits avec les plus gros rabais
 SELECT * from Produit p
 ORDER BY rabais DESC;
-#Produits les plus vendus
-SELECT p.nom, SUM(lc.quantite) AS total_vendu
-FROM LigneDeCommande lc
-JOIN Produit p ON lc.pid = p.pid
-GROUP BY p.pid
-ORDER BY total_vendu DESC;
-#Produits jamais commandés
-SELECT *
-FROM Produit
-WHERE pid NOT IN (
-    SELECT pid FROM LigneDeCommande
-);
-#Produits souvent commandés ensemble
+#Produits souvent commandés ensemble (Pas utilisée)
 SELECT
     p1.nom AS produit1,
     p2.nom AS produit2,
